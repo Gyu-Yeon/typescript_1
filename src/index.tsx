3c029acd3314f35d7b962c13f9d035678e83a77a
Gyu-Yeon <idsopy@nate.com>
2023-02-28 00:58:53 +0900

    7

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDom from "react-dom";
-import GuestList from "./state/GuestList";
+// import GuestList from "./state/GuestList";
 import UserSearch from "./refs/UserSearch";
 import EventComponent from "./state/events/EventComponents";
-import { Child, ChildAsFC } from "./props/Child";
+// import { Child, ChildAsFC } from "./props/Child";
 
 const App = () => {
   return (
